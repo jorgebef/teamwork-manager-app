9caefaabfacb52222c2672e7e2aee8ecab143ccc
Jorge Befan <jorge.befan@gmail.com>
2022-03-07 19:17:56 +0100

    adding  pages for creation

---
+++ pages/projects/index.tsx
@@ -0,0 +1,33 @@
+import { Box, Button, Typography } from "@mui/material";
+import { GetServerSideProps } from "next";
+
+const TestPage: React.FC = () => {
+  return (
+    <>
+      <Typography variant="h3">Projects</Typography>
+      <Button type="button" variant="contained">
+        Server Side Rendered Button
+      </Button>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+      <Box>alskdjñlaksdjf</Box>
+    </>
+  );
+};
+
+export const getServerSideProps = (ctx: GetServerSideProps) => {
+  return {
+    props: {}
+  };
+};
+
+export default TestPage;
