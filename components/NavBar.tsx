0f778c72cb5bf842d22278006911f91be4bacc4e
Jorge Befan <jorge.befan@gmail.com>
2022-03-20 21:24:06 +0100

    deploy to vercel with Next/Image for navbar and env variables

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -16,6 +16,7 @@ import {
 } from '@mui/material'
 import { Logout } from '@mui/icons-material'
 import Link from 'next/link'
+import Image from 'next/image'
 import SignInBtn from './SignInBtn'
 import DrawerToggleBtn from './DrawerToggleBtn'
 import { useAuthCtx } from './../context/AuthCtx'
@@ -93,10 +94,17 @@ const NavBar: React.FC = () => {
               <Tooltip title='Account'>
                 <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
                   <Avatar
-                    alt={user.displayName ? user.displayName : undefined}
-                    src={user.photoURL ? user.photoURL : undefined}
+                    // src={user.photoURL ? user.photoURL : undefined}
                     sx={{ height: 50, width: 50 }}
-                  ></Avatar>
+                  >
+                    <Image
+                      alt={user.displayName ? user.displayName : undefined}
+                      src={user.photoURL ? user.photoURL : profile3}
+                      layout='fill'
+                      // src={profile3}
+                      quality={20}
+                    />
+                  </Avatar>
                 </IconButton>
               </Tooltip>
               <Menu
