faae07bc62b43d9b7a59c1700ca0f9cfb5a0eed0
Jorge Befan <jorge.befan@gmail.com>
2022-04-01 20:15:19 +0200

    fixing usage of env variables in production

--- firebase/admin.ts
+++ firebase/admin.ts
@@ -7,7 +7,10 @@ if (!firebaseAdmin.apps.length) {
       credential: firebaseAdmin.credential.cert({
         projectId: process.env.NEXT_PUBLIC_PROJECTID,
         // Need to format the private key correctly because of dotenv
-        privateKey: process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+        privateKey:
+          process.env.NODE_ENV == 'production'
+            ? process.env.NEXT_PUBLIC_PRIVATE_KEY
+            : process.env.NEXT_PUBLIC_PRIVATE_KEY?.replace(/\\n/g, '\n'),
         clientEmail: process.env.NEXT_PUBLIC_CLIENT_EMAIL,
       }),
       databaseURL: `https://${process.env.NEXT_PUBLIC_PROJECTID}.firebaseio.com`,
