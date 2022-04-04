634ab9034cda0653737e30c2e62d2cc475799f69
Jorge Befan <jorge.befan@gmail.com>
2022-04-04 19:30:11 +0200

    extracting common data for easy modification

--- hooks/users.ts
+++ hooks/users.ts
@@ -1,33 +1,40 @@
 import {
   collection,
+  DocumentData,
   documentId,
   onSnapshot,
   query,
+  QueryDocumentSnapshot,
   where,
 } from 'firebase/firestore'
 import { useEffect, useState } from 'react'
 import { db } from '../firebase/config'
 import { IUser } from '../util/types'
 
+const userCollectionRef = collection(db, 'users')
+
+const docData = (doc: QueryDocumentSnapshot<DocumentData>) => {
+  return {
+    ...doc.data(),
+    uid: doc.data().uid,
+    userName: doc.data().userName,
+    email: doc.data().userName,
+    profilePic: doc.data().profilePic,
+    teams: doc.data().teams,
+    createdTasks: doc.data().createdTasks,
+    assignedTasks: doc.data().assignedTasks,
+  }
+}
+
 export const useUser = (uid: string | undefined) => {
   const [user, setUser] = useState<IUser>({} as IUser)
 
   useEffect(() => {
     if (!uid) return
-    const userCollectionRef = collection(db, 'users')
     const qUser = query(userCollectionRef, where(documentId(), '==', uid))
     const unsubscribe = onSnapshot(qUser, querySnapshot => {
       querySnapshot.forEach(doc => {
-        setUser({
-          ...doc.data(),
-          uid: doc.data().uid,
-          userName: doc.data().userName,
-          email: doc.data().userName,
-          profilePic: doc.data().profilePic,
-          teams: doc.data().teams,
-          createdTasks: doc.data().createdTasks,
-          assignedTasks: doc.data().assignedTasks,
-        })
+        setUser(docData(doc))
       })
     })
     return unsubscribe
@@ -41,24 +48,12 @@ export const useTeamUsers = (teamId: string | undefined | null) => {
 
   useEffect(() => {
     if (!teamId) return
-    const userCollectionRef = collection(db, 'users')
     const qUser = query(
       userCollectionRef,
       where('teams', 'array-contains', teamId)
     )
     const unsubscribe = onSnapshot(qUser, querySnapshot => {
-      setUsers(
-        querySnapshot.docs.map<IUser>(doc => ({
-          ...doc.data(),
-          uid: doc.data().uid,
-          userName: doc.data().userName,
-          email: doc.data().userName,
-          profilePic: doc.data().profilePic,
-          teams: doc.data().teams,
-          createdTasks: doc.data().createdTasks,
-          assignedTasks: doc.data().assignedTasks,
-        }))
-      )
+      setUsers(querySnapshot.docs.map<IUser>(doc => docData(doc)))
     })
     return unsubscribe
   }, [teamId])
